4c533a6e28ce7b8cfc1a82c45cf5136b169cca03
h3houk <daduzaxarov@gmail.com>
2018-02-14 21:39:24 +0400

    new

---
+++ Album2/js/main.js
@@ -0,0 +1,13 @@
+$('.slider__inner').slick({
+	autoplay: true,
+	arrows: false,
+	pauseOnHover: false,
+	pauseOnFocus: false
+});
+
+$("[data-fancybox]").fancybox({
+	slideShow : {
+		autoStart : true,
+		speed     : 4000
+	}
+	});
